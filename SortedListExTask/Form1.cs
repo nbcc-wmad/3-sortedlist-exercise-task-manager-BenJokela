58810cb4542736778841077539d7c049f62a746b
BenJokela <benjokela@gmail.com>
2019-04-30 15:13:33 -0300

    functioning

--- SortedListExTask/Form1.cs
+++ SortedListExTask/Form1.cs
@@ -17,13 +17,14 @@ namespace SortedListExTask
             InitializeComponent();
         }
         SortedList<DateTime, string> taskList = new SortedList<DateTime, string>();
-
+        
 
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
+        
 
         private void btnAddTask_Click(object sender, EventArgs e)
         {
@@ -33,12 +34,68 @@ namespace SortedListExTask
             }
             else
             {
+
                 DateTime taskDate = dtpTaskDate.Value;
-                string taskName = txtTask.Text.Trim();
+                if (taskList.ContainsKey(taskDate))
+                {
+                    MessageBox.Show("Only one task per date is allowed.", "Invalid Data", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    dtpTaskDate.Focus();
+                }
+                else
+                {
+                    string taskName = txtTask.Text.Trim();
 
-                taskList.Add(taskDate, taskName);
-                lstTasks.Items.Add(taskDate.ToShortDateString() + " " + taskName);
+                    taskList.Add(taskDate, taskName);
+                    lstTasks.Items.Add(taskDate);
+                    txtTask.Text = string.Empty;
+                    txtTask.Focus();
+                }
 
+            }
+        }
+
+        private void lstTasks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex != -1)
+            {
+                DateTime taskDate = (DateTime)lstTasks.SelectedItem;
+                var currentTask = taskList.Where(x => x.Key == taskDate).FirstOrDefault();
+                lblTaskDetails.Text = currentTask.Value.ToString();
+                //foreach (var x in taskList)
+                //{
+                //    if(x.Key == taskDate)
+                //    {
+                //        lblTaskDetails.Text = x.Value;
+                //    }
+                //}
+
+            }
+        }
+
+        private void btnPrintAll_Click(object sender, EventArgs e)
+        {
+            string message = string.Empty;
+            foreach(var x in taskList)
+            {
+                message += $"{x.Key} {x.Value}\n";
+            }
+            MessageBox.Show(message);
+        }
+
+        private void btnRemoveTask_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex == -1)
+            {
+                MessageBox.Show("You must select a task to remove.", "Invalid Data", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            else
+            {
+                DateTime taskDate = (DateTime)lstTasks.SelectedItem;
+                var currentTask = taskList.Where(x => x.Key == taskDate).FirstOrDefault();
+                taskList.Remove(currentTask.Key);
+                lstTasks.Items.Remove(lstTasks.SelectedItem);
+                lblTaskDetails.Text = string.Empty;
             }
         }
     }
